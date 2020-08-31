618e7c0c755c9f839783433e1038b7f32e2b7675
luox5005 <60677372+luox5005@users.noreply.github.com>
2020-08-31 05:56:22 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+import os
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-from flask import Flask, request
+from flask import Flask, render_template,request,flash
+import cv2
+from werkzeug.utils import secure_filename
+import datetime
+
+from cassandra.cluster import Cluster
+
+KEYSPACE = "data"
+cluster = Cluster(contact_points=['127.0.0.1'],port=9042)
+session = cluster.connect()
+
+try:
+    session.execute("create keyspace %s with replication = {'class': 'SimpleStrategy', 'replication_factor': 1};" % KEYSPACE)
+except:
+    pass
+session.set_keyspace('data')
+session.execute('use data')
+s = session
+try:
+    s.execute("CREATE TABLE data(uploadfilename text PRIMARY KEY,uploadtime text,prediction text)")
+except:
+    pass
 
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    req_time=datetime.datetime.now()
     if request.method == 'POST':
-        pic = request.files['file']
-        classify(pic)
-    return None
+        file = request.files['file']
 
-def classify(pic):
-    new_model = keras.models.load_model('C:/Users/luoxiang/saved_model/my_model')
-    probability_model = tf.keras.Sequential([
-        new_model,
-        tf.keras.layers.Softmax()
-    ])
-    image = tf.image.decode_jpeg(pic, channels = 1)
-    image = tf.image.resize(image, [28, 28])
-    im = np.array(image, dtype=int)
-    im = im.reshape(28, 28)
-    im = np.array([im])
-    predictions1 = probability_model.predict(im)
-    return np.argmax(predictions1[0])
+        upload_filename=secure_filename(file.filename)
+        save_filename=upload_filename
+        save_filepath=os.path.join(app.root_path,save_filename)
+        file.save(save_filepath)
 
+        new_model = keras.models.load_model('C:/Users/luoxiang/saved_model/my_model/my_picmodel.h5')
+        probability_model = tf.keras.Sequential([
+            new_model,
+            tf.keras.layers.Softmax()
+        ])
+        img=cv2.imread(save_filepath,0)
+        size=(28,28)
+        img_resize = cv2.resize(img, size)
+        img_resize = (np.expand_dims(img_resize,0))
+        predictions_single = probability_model.predict(img_resize)
+        np.argmax(predictions_single[0])
+
+        params = [save_filename,req_time.strftime('%Y-%m-%d %H:%M:%S.%f'),str(np.argmax(predictions_single[0]))]
+        s.execute("INSERT INTO data (uploadfilename,uploadtime,prediction)VALUES (%s, %s,%s)", params)
+
+        return("%s%s%s%s%s%s%s%s%s"%("Upload File Name:",save_filename,"\n","Upload Time:",req_time,"\n","Prediction:",np.argmax(predictions_single[0]),"\n"))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
